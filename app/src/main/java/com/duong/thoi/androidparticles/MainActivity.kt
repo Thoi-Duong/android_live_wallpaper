4aca5ebfb4f8c13e7dcaeae8e9b4e2883755de00
Thoi Duong <duong.thoi@1pac.vn>
2018-11-12 16:59:25 +0700

    init project

---
+++ app/src/main/java/com/duong/thoi/androidparticles/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.duong.thoi.androidparticles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
